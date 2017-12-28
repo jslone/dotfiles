3eb7f8edc9ba526d9044171045953af2753bb744
Jacob M. Slone <jslone1993@gmail.com>
2017-12-28 11:07:39 -0800

    Adding styling to xmonad - spacing, fake transparency and background

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,16 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
 
 main = xmonad =<< xmobar xmonadConfig
 
-
 xmonadConfig = defaultConfig
     { terminal    = "urxvt"
-    , borderWidth = 3
+    , borderWidth = 4
+    , normalBorderColor = "black"
+    , focusedBorderColor = "#bd93f9" --"#3AF0D1" -- "#FF1222" --"#69DFFA"   --"#E39402"    #00F2FF,
+    , startupHook = setWMName "LG3D"
+    , layoutHook = smartBorders $ smartSpacingWithEdge 5 $ Tall 1 (3/100) (1/2)
     }
